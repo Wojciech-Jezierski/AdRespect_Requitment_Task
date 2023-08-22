9f7f07fba3b827a74a05be46f13607f0c00165a8
Wojciech <wjezierski23@gmail.com>
2023-08-22 22:39:10 +0100

    Fix responsive mechanism

--- script.js
+++ script.js
@@ -91,6 +91,7 @@ const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
 
+
 const images = document.querySelectorAll('img')
 images.forEach(image => {
   image.addEventListener('click', e => {
